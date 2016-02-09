06bbe466e334351958168c75cfd57ee289c00f48
uwe <uwe@DX1704>
2016-02-09 23:07:30 +0100

    Updated copyright to show a dash between first and last year instead of a comma, which should be used when mentioning every single year.

--- src/de/dknapps/pswgendroid/ServiceDetailActivity.java
+++ src/de/dknapps/pswgendroid/ServiceDetailActivity.java
@@ -2,7 +2,7 @@
  * PswGen - Manages your websites and repeatably generates passwords for them
  * PswGenDroid - Generates your passwords managed by PswGen on your mobile  
  *
- *     Copyright (C) 2005, 2016 Uwe Damken
+ *     Copyright (C) 2005-2016 Uwe Damken
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
